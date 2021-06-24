f821b2fc4e4ea12e8d3173675ce6834133e02901
L Caraco <lbc.dna@gmail.com>
2021-06-23 21:16:29 -0700

    started basic rendering

--- src/topo/TopoApp.java
+++ src/topo/TopoApp.java
@@ -1,7 +1,22 @@
 package topo;
 
+import edu.macalester.graphics.CanvasWindow;
+import edu.macalester.graphics.Point;
+
 public class TopoApp {
+    public static final int WIDTH = 600, HEIGHT = 600;
+
+    public static final Point
+        DIMENSIONS = new Point(WIDTH, HEIGHT),
+        CENTER = DIMENSIONS.scale(0.5);
+
+    private final CanvasWindow canvas;
+
+    public TopoApp() {
+        canvas = new CanvasWindow("TopoApp", WIDTH, HEIGHT);
+        canvas.add(new TopoCurve(CENTER, 50));
+    }
     public static void main(String[] args) {
-        System.out.println("TopoApp");
+        new TopoApp();
     }
 }
